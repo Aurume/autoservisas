685a1d3eb91b8ed44524fd9951424a7928541cea
Aurelija Petrauskaite <aurelija35@gmail.com>
2023-01-24 14:17:15 +0200

    Veikia login, logout, slaptažodžio keitimas.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('servisas/', include('servisas.urls')),
     path('', RedirectView.as_view(url='servisas/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
